873801c69e9763026116a9082a7125ce1e57f3c7
Essensel <Essensel@yandex.ru>
2021-10-13 22:08:09 +0300

    исправлен Controller

--- src/main/java/BuilderController.java
+++ src/main/java/BuilderController.java
@@ -1,155 +1,40 @@
 import javafx.fxml.FXML;
- import javafx.event.ActionEvent;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 
 import javafx.scene.control.TextField;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 
 public class BuilderController {
 
-    @FXML
-    private Button multiButton;
-
-    @FXML
-    private Button resullButton;
-
-    @FXML
-    private Button zeroNumber;
-
-    @FXML
-    private Button cleanButton;
-
-    @FXML
-    private Button fiveNumber;
-
-    @FXML
-    private Button sixNumber;
-
-    @FXML
-    private Button twoNumber;
-
+    long numberOne;
+    long numberTwo;
     @FXML
     private TextField textField;
 
     @FXML
-    private Button oneNumber;
-
-    @FXML
-    private Button nineNumber;
-
-    @FXML
-    private Button plusButton;
-
-    @FXML
-    private Button fourNumber;
-
-    @FXML
-    private Button sevenNumber;
-
-    @FXML
-    private Button eightNumber;
-
-    @FXML
-    private Button minButton;
-
-    @FXML
-    private Button threeNumber;
-
-    @FXML
-    private Button splitButton;
-
-    @FXML
-    void num1(ActionEvent event) {
-String currentText = textField.getText();
-textField.setText(currentText +"1");
+    void number(ActionEvent event) {
+        int index = event.getSource().toString().length();
+        String text = Character.toString(event.getSource().toString().charAt(index - 2));
+        String resultText = textField.getText();
+        textField.setText(resultText + text);
     }
 
     @FXML
-    void num2(ActionEvent event) {
-        String currentText = textField.getText();
-        textField.setText(currentText +"2");
-    }
-
-    @FXML
-    void num3(ActionEvent event) {
-        String currentText = textField.getText();
-        textField.setText(currentText +"3");
-    }
-
-
-    @FXML
-    void num4(ActionEvent event) {
-        String currentText = textField.getText();
-        textField.setText(currentText +"4");
-    }
-
-    @FXML
-    void num5(ActionEvent event) {
-        String currentText = textField.getText();
-        textField.setText(currentText +"5");
-    }
-
-    @FXML
-    void num6(ActionEvent event) {
-        String currentText = textField.getText();
-        textField.setText(currentText +"6");
-    }
-
-    @FXML
-    void num7(ActionEvent event) {
-        String currentText = textField.getText();
-        textField.setText(currentText +"7");
-    }
-
-    @FXML
-    void num8(ActionEvent event) {
-        String currentText = textField.getText();
-        textField.setText(currentText +"8");
-    }
-
-    @FXML
-    void num9(ActionEvent event) {
-        String currentText = textField.getText();
-        textField.setText(currentText +"9");
-    }
-
-    @FXML
-    void num10(ActionEvent event) {
-        String currentText = textField.getText();
-        textField.setText(currentText +"0");
-    }
-    @FXML
-    void plusOperation(ActionEvent event) {
+    void operation(ActionEvent event) {
+        if (!textField.getText().equals("")) {
+            numberOne = Long.parseLong(textField.getText());
+            System.out.println(numberOne);
+        }
+        int index = event.getSource().toString().length();
+        String text = Character.toString(event.getSource().toString().charAt(index - 2));
+        textField.setText(text);
 
     }
-    @FXML
-    void minusOperation(ActionEvent event) {
-
-    }
-
-
-    @FXML
-    void multiOperation(ActionEvent event) {
-
-    }
-
-    @FXML
-    void resulltOperation(ActionEvent event) {
-
-    }
-
     @FXML
     void cleanOperation(ActionEvent event) {
-textField.setText("");
+        textField.setText("");
     }
-
-
-    @FXML
-    void splitOperation(ActionEvent event) {
-
-    }
-
 }
+
+
