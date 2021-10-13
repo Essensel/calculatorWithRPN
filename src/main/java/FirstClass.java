17ee3230c6924eebcbb9acef7077d09043f52b80
Essensel <Essensel@yandex.ru>
2021-10-13 15:43:01 +0300

    пробный commit

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,5 +1,24 @@
-public class FirstClass {
-    public static void main(String[] args) {
-        System.out.println("ВЫВОД НА ЭКРАН");
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+    public class FirstClass extends Application {
+
+        public static void main(String[] args) throws Exception {
+            launch(args);
+        }
+
+        @Override
+        public void start(Stage stage) throws Exception {
+            String fxmlFile = "builder.fxml";
+            FXMLLoader loader = new FXMLLoader();
+            Parent root = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+            stage.setTitle("Vishnyakov.APP");
+            stage.setScene(new Scene(root));
+            stage.show();
+            System.out.println("Hello");
+        }
     }
-}
