68aef45683f25cdd0d75b7b362ce338e436ffec0
Essensel <Essensel@yandex.ru>
2021-10-16 21:58:38 +0300

    реализован калькулятор(кривое отображение)

--- src/main/java/BuilderController.java
+++ src/main/java/BuilderController.java
@@ -1,39 +1,81 @@
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+
+import java.awt.*;
 
 
 public class BuilderController {
+    private CulcModel culcModel = new CulcModel();
+    private Double numberOne;
+    private Double numberTwo;
+    private String numOneSt = "";
+    private String numTwoSt = "";
+    String operationSymbol = "";
+    String curentResult = "";
 
-    long numberOne;
-    long numberTwo;
     @FXML
     private TextField textField;
+    @FXML
+    private TextField curentTextField;
 
     @FXML
     void number(ActionEvent event) {
+
+        String resultText = textField.getText();
         int index = event.getSource().toString().length();
         String text = Character.toString(event.getSource().toString().charAt(index - 2));
-        String resultText = textField.getText();
-        textField.setText(resultText + text);
+
+        if ((!resultText.contains("+")) & (!resultText.contains("-")) & (!resultText.contains("/")) & (!resultText.contains("X"))) {
+            textField.setText(resultText + text);
+        } else {
+            textField.setText(text);
+        }
+        if (!numOneSt.equals("") && !operationSymbol.equals("")) {
+            Double result;
+            numTwoSt = textField.getText();
+            numberOne = Double.parseDouble(numOneSt);
+            numberTwo = Double.parseDouble(numTwoSt);
+            result = culcModel.mathOperation(numberOne, numberTwo, operationSymbol);
+            curentResult = result.toString();
+            curentTextField.setText(curentResult);
+        }
     }
 
     @FXML
     void operation(ActionEvent event) {
-        if (!textField.getText().equals("")) {
-            numberOne = Long.parseLong(textField.getText());
-            System.out.println(numberOne);
-        }
-        int index = event.getSource().toString().length();
-        String text = Character.toString(event.getSource().toString().charAt(index - 2));
-        textField.setText(text);
 
+        String resultText = textField.getText();
+        if(curentResult.equals("")){
+        if (numOneSt.equals("")) {
+            numOneSt = resultText;
+        } else {
+            numTwoSt = resultText;
+        }} else {
+            numOneSt = curentResult;
+        }
+
+        if ((!resultText.equals("")) & (!resultText.contains("-")) & (!resultText.contains("+")) & (!resultText.contains("/")) & (!resultText.contains("X"))) {
+            int index = event.getSource().toString().length();
+            String text = Character.toString(event.getSource().toString().charAt(index - 2));
+            textField.setText(text);
+            operationSymbol = text;
+            curentTextField.setText(numOneSt + " " + operationSymbol);
+        }
     }
+
     @FXML
     void cleanOperation(ActionEvent event) {
         textField.setText("");
+        curentTextField.setText("");
+        numberOne=0.0;
+        numberTwo=0.0;
+         numOneSt = "";
+         numTwoSt = "";
+       operationSymbol = "";
+       curentResult = "";
     }
 }
 
